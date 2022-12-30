296a54594b803a70b3a2ab58fdbbb82e85bf17f4
toothemooon <35186738+toothemooon@users.noreply.github.com>
2022-12-30 21:54:15 +0800

    _form

---
+++ src/AddPersonForm.svelte
@@ -0,0 +1,21 @@
+<script>
+  let name;
+  let beltColor;
+  let age;
+
+  const handleSubmit = () => {
+    console.log(name, beltColor, age);
+  }
+</script>
+
+<!-- submit form(on:click on button -->
+<form on:submit|preventDefault={handleSubmit}>
+    <input type="text" placeholder="name" bind:value={name}>
+    <input type="text" placeholder="belt color" bind:value={beltColor}>
+    <input type="number" placehodler="age" bind:value={age}>
+    <button>Add Person</button>
+</form>
+
+<style>
+
+</style>
--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,70 @@
 <script>
-  export let name;
-  let beltColor = 'just getting started with the basics';
+  // Event modifiers:
+  // once - makes sure the event can only fire once (removes handler)
+  // preventDefault - prevent the default action (run e.preventDefault())
+  // self - only fires the event if the clicked element is the target
+  import Modal from './Modal.svelte';
+  import AddPersonForm from './AddPersonForm.svelte';
 
-  const handleClick = () => {
-    beltColor = 'orange belt';
+  // override the showModal props
+  let showModal = false;
+
+  const toggleModal = () => {
+    showModal = !showModal;
   }
 
-  console.log("Hello~");
+  let people = [
+    { name: 'yoshi', beltColor: 'black', age: 25, id:1 },
+    { name: 'mario', beltColor: 'red', age: 30, id: 2},
+    { name: 'luigi', beltColor: 'purple', age: 30, id:3}
+  ];
+
+  const handleClick = (id) => {
+    people = people.filter((person) => person.id != id);
+  }
+
+  let num = 20;
+
 </script>
 
+<!-- <Modal message="Hey I am a prop value" isPromo=(true)/> -->
+<!-- event forwarding -->
+<!-- slot -->
+<Modal showModal={showModal} on:click={toggleModal}>
+  <AddPersonForm />
+</Modal>
+
+
+{#if num > 15} 
+  <h3 class="Shishou">Hello Shishou~</h3>
+{:else if num > 10}
+  <p>num greater than 10</p> 
+{:else}
+  <p>num less or equal than 10</p>
+{/if}
 <main>
-  <h1>Hello {name}~</h1>
-  <p>{beltColor}</p> 
-  <button on:click={handleClick}>update above text</button>
+  <button on:click|once={toggleModal}>open modal</button>
+  {#each people as person}
+  <div>
+    <h4>{person.name}</h4>
+    {#if person.beltColor == 'black'}
+      <p><strong>master ninja</strong></p>
+    {/if}
+    <p>{person.age} years old, {person.beltColor} belt</p>
+    <button on:click={handleClick(person.id)}>delete</button>
+  </div>
+  {:else}
+    <p>no people to show</p>
+  {/each}
 </main>
 
+<!-- component specific style by using unique classes -->
 <style>
   main {
     text-align: center;
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
   }
 
   @media (min-width: 640px) {
